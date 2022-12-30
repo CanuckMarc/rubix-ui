f9a08eb8ad7ee365591f3c5a36fcc65ae53b235c
hanh.tran <hanh.tv204@gmail.com>
2022-12-30 22:23:50 +0700

    fix: import nodes

--- frontend/src/components/rubix-flow/components/LoadModal.tsx
+++ frontend/src/components/rubix-flow/components/LoadModal.tsx
@@ -7,7 +7,11 @@ import { Modal } from "./Modal";
 import { GraphJSON } from "../lib";
 import { handleNodesEmptySettings } from "../util/handleSettings";
 import { useParams } from "react-router-dom";
-import { handleCopyNodesAndEdges } from "../util/handleNodesAndEdges";
+import { NodeInterface } from "../lib/Nodes/NodeInterface";
+import { generateUuid } from "../lib/generateUuid";
+import { Edge } from "react-flow-renderer";
+
+type NodeInterfaceWithOldId = NodeInterface & { oldId?: string };
 
 export type LoadModalProps = {
   open?: boolean;
@@ -45,14 +49,51 @@ export const LoadModal: FC<LoadModalProps> = ({ open = false, onClose }) => {
 
     // add nodes into sub flow if have
     if (window.selectedNodeForSubFlow) {
-      nodes = nodes.map((node) => ({ ...node, parentId: window.selectedNodeForSubFlow!!.id, isParent: false }));
+      nodes = nodes.map((node) => ({
+        ...node,
+        id: generateUuid(),
+        parentId: window.selectedNodeForSubFlow!!.id,
+        oldId: node.id,
+        isParent: false,
+      }));
+    } else {
+      // filter parent nodes and nodes not belonging to sub flow
+      // generate nodes with new id and keep old id to mapping with edge
+      const newNodes: NodeInterfaceWithOldId[] = [];
+      const parentNodes = nodes.filter((node: NodeInterface) => node.isParent);
+      const remainingNodes = nodes.filter((node: NodeInterface) => !node.isParent && !node.parentId);
+
+      parentNodes.forEach((parentNode: NodeInterface) => {
+        const newNodeId = generateUuid();
+        const childNodes = nodes.filter((node: NodeInterface) => parentNode.id === node.parentId);
+        if (childNodes.length > 0) {
+          childNodes.forEach((childNode: NodeInterface) => {
+            newNodes.push({ ...childNode, oldId: childNode.id, id: generateUuid(), parentId: newNodeId });
+          });
+        }
+        newNodes.push({ ...parentNode, id: newNodeId, oldId: parentNode.id });
+      });
+
+      newNodes.push(
+        ...remainingNodes.map((node: NodeInterface) => ({
+          ...node,
+          oldId: node.id,
+          id: generateUuid(),
+        }))
+      );
+      nodes = newNodes;
     }
 
-
-    // Create new uuid for nodes and edges
-    const newFlow = handleCopyNodesAndEdges({ nodes, edges });
-    nodes = newFlow.nodes;
-    edges = newFlow.edges;
+    edges = edges.map((edge: Edge) => {
+      const target = nodes.find((n: any) => n.oldId === edge.target);
+      const source = nodes.find((n: any) => n.oldId === edge.source);
+      return {
+        ...edge,
+        id: generateUuid(),
+        target: target?.id || edge.target,
+        source: source?.id || edge.source,
+      };
+    });
 
     instance.setNodes([...oldNodes, ...nodes]);
     instance.setEdges([...oldEdges, ...edges]);
