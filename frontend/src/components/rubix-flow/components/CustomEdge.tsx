e7bbc2d2f47c1e3010f4d94271a549e1584ba980
hanh.tran <hanh.tv204@gmail.com>
2023-01-09 14:49:30 +0700

    fix position of inputs in parent node

--- frontend/src/components/rubix-flow/components/CustomEdge.tsx
+++ frontend/src/components/rubix-flow/components/CustomEdge.tsx
@@ -60,7 +60,7 @@ export const CustomEdge = (props: EdgeProps & { parentNodeId?: string }) => {
       const newSourceY = sourceIndex >= 0 && parentNode ? parentNode!!.position.y + (70 + sourceIndex * 32) : sourceY;
       const newSourcePosition = sourceIndex >= 0 ? Position.Right : sourcePosition;
       const newTargetX = targetIndex >= 0 && parentNode ? parentNode!!.position.x - 7 : targetX;
-      const newTargetY = targetIndex >= 0 && parentNode ? parentNode!!.position.y + (70 + targetIndex * 32) : targetY;
+      const newTargetY = targetIndex >= 0 && parentNode ? parentNode!!.position.y + (48 + targetIndex * 32) : targetY;
       const newTargetPosition = targetIndex >= 0 ? Position.Left : targetPosition;
 
       setPath(
