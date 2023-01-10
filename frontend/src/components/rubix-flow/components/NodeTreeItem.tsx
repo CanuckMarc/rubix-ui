b18e198a3654153ac1eb55221110aad37b4bb063
hau.trinh <hau.trinh@digitalfortress.dev>
2023-01-10 12:16:48 +0700

    add in the node name

--- frontend/src/components/rubix-flow/components/NodeTreeItem.tsx
+++ frontend/src/components/rubix-flow/components/NodeTreeItem.tsx
@@ -13,14 +13,14 @@ export const NodeTreeItem = ({ allNodes, childNodes }: NodeProps) => {
     <Collapse expandIconPosition="right" className="ant-menu ant-menu-root ant-menu-inline ant-menu-dark border-0">
       {childNodes?.map((node, index) => {
         const nodesChild: NodeInterface[] = allNodes.filter((item: NodeInterface) => item.parentId === node.id);
-        const type = node.type!!.split("/")[1];
+        const type = node.type!!.split("/")[1] + (node.info && (node.info.nodeName !== "") ? ` (${node.info?.nodeName})` : "");
 
         return node.isParent ? (
           <Panel
             key={index}
             header={type}
-            className="panel-no-padding border-gray-600"
-            style={{ paddingLeft: !node.parentId ? undefined : 24 }}
+            className={`panel-no-padding border-gray-600 border-t border-b-0 `}
+            style={{ paddingLeft: !node.parentId ? undefined : 10 }}
           >
             <div className="bg-gray-800">
               <NodeTreeItem childNodes={nodesChild} allNodes={allNodes} />
@@ -29,9 +29,8 @@ export const NodeTreeItem = ({ allNodes, childNodes }: NodeProps) => {
         ) : (
           <div
             key={index}
-            className={`py-2 cursor-po inter flex flex-row justify-between text-left ant-menu-item border-gray-600 ${
-              index === 0 ? "" : "border-t"
-            }`}
+            className={`py-2 cursor-po inter flex flex-row justify-between text-left ant-menu-item border-gray-600 ${index === 0 ? "" : "border-t"}`}
+            style={{ paddingLeft: !node.parentId ? undefined : 26 }}
           >
             <div>{type}</div>
           </div>
