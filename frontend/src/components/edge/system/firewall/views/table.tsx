45b1d9fbdff8ae5bfa3ee1d4ee3382e7af8cb747
Jia Fei <jia_fei1991@hotmail.com>
2023-01-13 10:31:52 +1100

    working table without changing status

--- frontend/src/components/edge/system/firewall/views/table.tsx
+++ frontend/src/components/edge/system/firewall/views/table.tsx
@@ -1,47 +1,45 @@
-import { Button, Descriptions, Modal, Table, Spin, Space, Switch } from "antd";
+import { Button, Modal, Table, Spin, Space, Switch, Input } from "antd";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { HostFirewallFactory } from "../factory";
-import { ReloadOutlined, MinusOutlined } from '@ant-design/icons';
+import { ReloadOutlined, PlusOutlined, MinusOutlined } from '@ant-design/icons';
+import { system, ufw } from "../../../../../../wailsjs/go/models";
 import type { ColumnsType } from 'antd/es/table';
 
 interface PortTableType {
   key: number;
-  port: string;
+  port: number;
   status: string;
 }
 
 export const Firewall = () => {
   const { connUUID = "", hostUUID = "" } = useParams();
-  const [firewallList, setFirewallList] = useState([] as any);
+  const [firewallList, setFirewallList] = useState([] as PortTableType[]);
   const [isFetching, setIsFetching] = useState(false);
-  const [data, setData] = useState(
-    [
-      {
-        key: 0,
-        port: '3000',
-        status: 'open'
-      },
-      {
-        key: 1,
-        port: '3001',
-        status: 'close'
-      },
-      {
-        key: 2,
-        port: '3000',
-        status: 'open'
-      },
-      {
-        key: 3,
-        port: '3001',
-        status: 'close'
-      }
-    ]
-  )
 
   const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const [firewallStatus, setFirewallStatus] = useState(false);
+  const [selectedRow, setSelectedRow] = useState({} as PortTableType)
   const [loading, setLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [newPort, setNewPort] = useState("")
+
+
+  const handleOk = () => {
+    const newData = [...firewallList, {
+      key: Date.now() + Number(newPort),
+      port: Number(newPort),
+      status: "close"
+    }]
+    setFirewallList(newData)
+    setNewPort("");
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setNewPort("");
+    setIsModalOpen(false);
+  };
 
   const factory = new HostFirewallFactory();
 
@@ -49,17 +47,21 @@ export const Firewall = () => {
     {
       title: 'Port',
       dataIndex: 'port',
-      key: 'port'
+      key: 'port',
+      // width: '20vw',
+      fixed: "left"
     },
     {
       title: 'Status',
       dataIndex: 'status',
-      key: 'status'
+      key: 'status',
+      // width: '20vw'
     },
     {
       title: 'Action',
       dataIndex: 'action',
       key: 'action',
+      // width: '20vw',
       render: () => (
         <Space size="middle">
           <Switch defaultChecked={false} onChange={onTogglePort}/>
@@ -75,9 +77,27 @@ export const Firewall = () => {
   const fetch = async () => {
     try {
       setIsFetching(true);
-      const res = await factory.EdgeFirewallList(connUUID, hostUUID);
-      setFirewallList(res);
-      console.log(res)
+      const status = await factory.EdgeFirewallStatus(connUUID, hostUUID);
+      
+      if (status != null) {
+        // TODO: just guessing the message content
+        setFirewallStatus(status.message == "open" ? true : false);
+      }
+      
+      const list = await factory.EdgeFirewallList(connUUID, hostUUID);
+      let mappedList: PortTableType[] = []
+      if (list != null) {
+        mappedList = list.map( item => {
+          return {
+            // TODO: need to test if this works with fetching a list
+            key: Date.now() + item.port,
+            port: item.port,
+            status: item.status
+          }
+        })
+      }
+      setFirewallList(mappedList);
+      console.log(mappedList)
     } catch (error) {
       console.log(error);
     } finally {
@@ -85,7 +105,7 @@ export const Firewall = () => {
     }
   };
 
-  const onToggleChange = async(checked: boolean) => {
+  const onFirewallEnableToggle = async(checked: boolean) => {
     if (checked) {
         await factory.EdgeFirewallEnable(connUUID, hostUUID)
     } else {
@@ -93,9 +113,16 @@ export const Firewall = () => {
     }
   }
 
-  const onTogglePort = (checked: boolean, event: Event) => {
+  const onTogglePort = async (checked: boolean) => {
     console.log(checked)
-
+    const body = new system.UFWBody({port: selectedRow.port})
+    let res: ufw.Message
+    if (checked) {
+      res = await factory.EdgeFirewallPortOpen(connUUID, hostUUID, body)
+    } else {
+      res = await factory.EdgeFirewallPortClose(connUUID, hostUUID, body)
+    }
+    console.log(res);
   }
 
   const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
@@ -112,35 +139,64 @@ export const Firewall = () => {
 
   const deleteSelectedPorts = () => {
     console.log(selectedRowKeys)
-    const filteredData = data.filter(item => {
+    const filteredData = firewallList.filter(item => {
       let flag = false
       selectedRowKeys.forEach( (value, i) => {
         if (item.key == value) flag = true
       })
       return (flag == false) && item
     })
-    setData(filteredData)
+    setFirewallList(filteredData)
     setSelectedRowKeys([])
+  }
+
+  const addPort = () => {
+    setIsModalOpen(true)
+  }
+
+  const onPortInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewPort(event.target.value)
   }
 
   return (
     <>
-      <Space align="start" direction="vertical">
+      <Space align="start" direction="vertical" style={{ width: "100%" }}>
         <Space align="center" direction="horizontal">
           <Button type="primary" icon={<ReloadOutlined />} size={"middle"}>Refresh</Button>
+          <Button type="primary" icon={<PlusOutlined />} onClick={addPort} loading={loading}>
+            Add port
+          </Button>
           <Button type="primary" danger={true} icon={<MinusOutlined />} onClick={deleteSelectedPorts} disabled={!hasSelected} loading={loading}>
             Delete port
           </Button>
         </Space>
         <Spin spinning={isFetching}>
-          <Table columns={columns} dataSource={data} rowSelection={rowSelection} onRow={(rowIndex) => {
-            return {
-              onClick: () => {console.log(rowIndex)} // click row
-            };
-          }}/>
+          <Table columns={columns} dataSource={firewallList} rowSelection={rowSelection} style={{ tableLayout: "auto", width: "76.5vw" }}
+            onRow={(row) => {
+              return {
+                onClick: () => {setSelectedRow(row)} // click row
+              };
+            }} 
+            pagination={{
+              position: ["bottomLeft"],
+              showSizeChanger: true,
+              pageSizeOptions: [10, 50, 100, 1000],
+              locale: { items_per_page: "" },
+            }}
+            scroll={{ x: "max-content" }}/>
         </Spin>
-        <Switch defaultChecked={false} onChange={onToggleChange} />
+        <Space align="center" direction="horizontal">
+          <span>Enable firewall: </span>
+          <Switch defaultChecked={firewallStatus} onChange={onFirewallEnableToggle} />
+        </Space>
       </Space>
+
+      <Modal title="Add Port" visible={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <Space align="center" direction="horizontal">
+          <span>Port number: </span>
+          <Input value={newPort} onChange={onPortInputChange}/>
+        </Space>
+      </Modal>
     </>
   );
 };
