6702ec1ebdfda121541dcb3d4e789cb6ae68d535
RaiBnod <binodrayee@gmail.com>
2023-01-30 17:09:38 +0545

    Proper use of async/await

--- frontend/src/components/hostNetworks/factory.tsx
+++ frontend/src/components/hostNetworks/factory.tsx
@@ -31,85 +31,37 @@ export class NetworksFactory {
     return this.count;
   }
 
-  // get the first connection uuid
-  async GetFist(): Promise<amodel.Network> {
-    let one: amodel.Network = {} as amodel.Network;
-    await this.GetAll()
-      .then((res) => {
-        one = res.at(0) as amodel.Network;
-        this._this = one;
-      })
-      .catch((err) => {
-        return undefined;
-      });
-    return one;
-  }
-
   async GetAll(): Promise<Array<amodel.Network>> {
-    let all: Array<amodel.Network> = {} as Array<amodel.Network>;
-    await GetHostNetworks(this.connectionUUID)
-      .then((res) => {
-        all = res as Array<amodel.Network>;
-      })
-      .catch((err) => {
-        return undefined;
-      });
-    return all;
+    return GetHostNetworks(this.connectionUUID)
   }
 
   async GetOne(): Promise<amodel.Network> {
     hasUUID(this.uuid);
-    let one: amodel.Network = {} as amodel.Network;
-    await GetHostNetwork(this.connectionUUID, this.uuid)
-      .then((res) => {
-        one = res as amodel.Network;
-        this._this = one;
-      })
-      .catch((err) => {
-        return undefined;
-      });
+    const res = await GetHostNetwork(this.connectionUUID, this.uuid)
+    const one = res as amodel.Network;
+    this._this = one;
     return one;
   }
 
   async Add(): Promise<amodel.Network> {
     hasUUID(this.connectionUUID);
-    let one: amodel.Network = {} as amodel.Network;
-    await AddHostNetwork(this.connectionUUID, this._this)
-      .then((res) => {
-        one = res as amodel.Network;
-        this._this = one;
-      })
-      .catch((err) => {
-        return undefined;
-      });
+    const res = await AddHostNetwork(this.connectionUUID, this._this)
+    const one = res as amodel.Network;
+    this._this = one;
     return one;
   }
 
   async Update(): Promise<amodel.Network> {
     hasUUID(this.uuid);
-    let one: amodel.Network = {} as amodel.Network;
-    await EditHostNetwork(this.connectionUUID, this.uuid, this._this)
-      .then((res) => {
-        one = res as amodel.Network;
-        this._this = one;
-      })
-      .catch((err) => {
-        return undefined;
-      });
+    const res = await EditHostNetwork(this.connectionUUID, this.uuid, this._this)
+    const one = res as amodel.Network;
+    this._this = one;
     return one;
   }
 
   async BulkDelete(uuids: Array<backend.UUIDs>): Promise<any> {
     hasUUID(this.connectionUUID);
-    let out: Promise<any> = {} as Promise<any>;
-    await DeleteHostNetworkBulk(this.connectionUUID, uuids)
-      .then((res) => {
-        out = res as Promise<any>;
-      })
-      .catch((err) => {
-        return undefined;
-      });
-    return out;
+    return await DeleteHostNetworkBulk(this.connectionUUID, uuids)
   }
 
   async Schema(): Promise<any> {
