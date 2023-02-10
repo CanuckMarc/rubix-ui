b678be9b405982cb5ceb492055dffe1d8e5be0d0
Ritesh Shakya <riteshakya037@gmail.com>
2023-02-10 22:16:38 +0400

    Add DebounceInput and TokenLoginInput

---
+++ frontend/src/common/token-form-input.tsx
@@ -0,0 +1,148 @@
+import { Button, Form, Col, Input, Row, Tooltip } from "antd";
+import { FormInstance, Rule, } from "antd/lib/form";
+import { ArrowRightOutlined, ScanOutlined, ArrowLeftOutlined, LoadingOutlined } from "@ant-design/icons";
+import { useState, useEffect } from "react";
+
+const { Search } = Input;
+
+export interface TokenFormInputProps {
+    name: string;
+    form: FormInstance<any>;
+    label?: string;
+    placeholder?: string;
+    rules?: Rule[];
+    onCall: (username: string, password: string) => Promise<TokenFormLoginResponse>;
+}
+
+export interface TokenFormLoginResponse {
+    token?: string,
+    success: boolean,
+}
+
+export const TokenFormInput = (props: TokenFormInputProps) => {
+    const [useAuth, setUseAuth] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [token, setToken] = useState("");
+
+    const [form] = Form.useForm();
+    const [, forceUpdate] = useState({});
+
+    const toggleAuth = () => {
+        setUseAuth(!useAuth)
+    }
+
+    useEffect(() => {
+        props.form.setFieldsValue({
+            [props.name]: token
+        })
+    }, [useAuth]);
+
+    const onLogin = () => {
+        setIsLoading(true)
+        const username = form.getFieldValue("username")
+        const password = form.getFieldValue("password")
+
+        props.onCall(username, password).then((response) => {
+            setIsLoading(false)
+            if (response.success) {
+                setToken(response.token ?? "")
+                setUseAuth(false)
+            }
+        })
+    }
+
+    return useAuth ?
+        <Form.Item label={props.label} required>
+            <Form
+                form={form}
+                onFinish={onLogin}
+                component={false}
+            >
+                <Row align="top" style={{ maxHeight: 32 }} wrap={false}>
+                    <Col flex="none">
+                        <Form.Item
+                            help=""
+                            validateStatus="">
+                            <Tooltip title="Go back to manual entry">
+                                <Button icon={<ArrowLeftOutlined />} onClick={toggleAuth} />
+                            </Tooltip>
+                        </Form.Item>
+                    </Col>
+                    <Col flex="auto">
+                        <Form.Item
+                            name="username"
+                            help=""
+                            validateStatus=""
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your username!'
+                                }
+                            ]}
+                        >
+                            <Input placeholder="Username" />
+                        </Form.Item>
+                    </Col>
+                    <Col flex="auto">
+                        <Form.Item
+                            name="password"
+                            help=""
+                            validateStatus=""
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your password!'
+                                }
+                            ]}
+                        >
+                            <Input.Password
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </Form.Item>
+                    </Col>
+                    <Col flex="none">
+                        <Form.Item shouldUpdate>
+                            {() => <Tooltip title="Login">
+                                <Button
+                                    icon={isLoading ? <LoadingOutlined /> : <ArrowRightOutlined />}
+                                    onClick={onLogin}
+                                    disabled={
+                                        !form.isFieldsTouched(true) ||
+                                        !!form.getFieldsError().filter(({ errors }) => errors.length).length
+                                    }
+                                />
+                            </Tooltip>
+                            }
+                        </Form.Item>
+                    </Col>
+                </Row>
+            </Form>
+        </Form.Item>
+        :
+        <Row align="bottom">
+            <Col flex="auto">
+                <Form.Item
+                    name={props.name}
+                    label={props.label}
+                    rules={props.rules}
+                    initialValue={token}
+                    shouldUpdate={(prevValues, curValues) => {
+                        return prevValues.token !== curValues.token;
+                    }}
+                >
+                    <Input
+                        placeholder={props.placeholder}
+                    >
+                    </Input>
+                </Form.Item>
+            </Col>
+            <Col flex="32px">
+                <Form.Item>
+                    <Tooltip title="Login to generate token">
+                        <Button icon={<ScanOutlined />} onClick={toggleAuth} />
+                    </Tooltip>
+                </Form.Item>
+            </Col>
+        </Row >
+}
--- frontend/src/components/hosts/host/flow/flowNetworks/networks/views/create.tsx
+++ frontend/src/components/hosts/host/flow/flowNetworks/networks/views/create.tsx
@@ -1,75 +1,129 @@
-import { Modal } from "antd";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { model } from "../../../../../../../../wailsjs/go/models";
-
-import { JsonForm } from "../../../../../../../common/json-schema-form";
 import { FlowFrameworkNetworkFactory } from "../factory";
 import { useParams } from "react-router-dom";
-import { LOCAL_FLOW_NETWORKS_SCHEMA, REMOTE_FLOW_NETWORKS_SCHEMA } from "../../../../../../../constants/headers";
 import FlowNetwork = model.FlowNetwork;
+import { Checkbox, Form, Input, Modal } from "antd";
+import { DebounceInputForm } from "../../../../../../../common/debounce-form-input";
+import { TokenFormInput } from "../../../../../../../common/token-form-input";
+
 
 export const CreateEditModal = (props: any) => {
+  const [form] = Form.useForm();
+
   const { currentItem, isModalVisible, refreshList, onCloseModal } = props;
   const { connUUID = "", hostUUID = "" } = useParams();
   const [confirmLoading, setConfirmLoading] = useState(false);
-  const [formData, setFormData] = useState(currentItem);
-  const [schema, setSchema] = useState({});
 
   const factory = new FlowFrameworkNetworkFactory();
   factory.connectionUUID = connUUID;
   factory.hostUUID = hostUUID;
 
-  useEffect(() => {
-    setFormData(currentItem);
-  }, [isModalVisible]);
+  const _onCloseModal = () => {
+    form.resetFields()
+    onCloseModal()
+  }
 
-  useEffect(() => {
-    if (formData.is_remote) {
-      setSchema(REMOTE_FLOW_NETWORKS_SCHEMA);
-    } else {
-      setSchema(LOCAL_FLOW_NETWORKS_SCHEMA);
-    }
-  }, [formData.is_remote]);
-
-  const handleSubmit = async (network: FlowNetwork) => {
-    network.flow_https = false;
-    network.flow_https_local = false;
-    network.is_token_auth = true;
-    network.is_master_slave = false;
-    try {
-      setConfirmLoading(true);
-      if (currentItem.uuid) {
-        network.uuid = currentItem.uuid;
-        await factory.Update(network.uuid, network);
-      } else {
-        await factory.Add(network);
-      }
-      refreshList();
-      onCloseModal();
-    } catch (error) {
-    } finally {
-      setConfirmLoading(false);
-    }
+  const handleSubmit = async () => {
+    const network = form.validateFields
+    console.log("flow_token_local", form.getFieldValue("flow_token_local"))
+    // network.flow_https = false;
+    // network.flow_https_local = false;
+    // network.is_token_auth = true;
+    // network.is_master_slave = false;
+    // try {
+    //   setConfirmLoading(true);
+    //   if (currentItem.uuid) {
+    //     network.uuid = currentItem.uuid;
+    //     await factory.Update(network.uuid, network);
+    //   } else {
+    //     await factory.Add(network);
+    //   }
+    //   refreshList();
+    //   onCloseModal();
+    // } catch (error) {
+    // } finally {
+    //   setConfirmLoading(false);
+    // }
   };
+
+  const showAdditionalFields = Form.useWatch('is_remote', form) == true;
 
   return (
     <>
       <Modal
         title={currentItem.uuid ? "Edit " + currentItem.name : "Add"}
         visible={isModalVisible}
-        onOk={() => handleSubmit(formData)}
-        onCancel={onCloseModal}
+        onOk={() => handleSubmit()}
+        onCancel={_onCloseModal}
         confirmLoading={confirmLoading}
         okText="Save"
         maskClosable={false}
         style={{ textAlign: "start" }}
       >
-        <JsonForm
-          formData={formData}
-          setFormData={setFormData}
-          handleSubmit={handleSubmit}
-          jsonSchema={schema}
-        />
+        <Form
+          form={form}
+          layout="vertical"
+          onSubmitCapture={handleSubmit}
+        >
+          <Form.Item
+            name="name"
+            label="Name"
+            rules={[
+              {
+                required: true,
+                message: 'Please input the Name!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item name="is_remote" valuePropName="checked">
+            <Checkbox>Is Remote Network</Checkbox>
+          </Form.Item>
+          {
+            showAdditionalFields && <>
+              <DebounceInputForm
+                name="flow_ip_remote"
+                label="Flow IP Remote"
+                placeholder="1.1.1.1"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Flow IP Remote is a required field.',
+                  }
+                ]}
+                onCall={(url: string) => Promise.resolve({ success: true })}
+              />
+              <TokenFormInput
+                form={form}
+                name="flow_token_local"
+                label="Flow Token Local"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Flow IP Remote is a required field.',
+                  }
+                ]}
+                onCall={(username: string, password: string) => Promise.resolve({ token: "this is token", success: true })}
+              />
+
+              <TokenFormInput
+                form={form}
+                name="flow_token_remote"
+                label="Flow Token Remote"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Flow IP Remote is a required field.',
+                  }
+                ]}
+                onCall={(username: string, password: string) => Promise.resolve({ token: "this is token", success: true })}
+              />
+            </>
+          }
+        </Form>
       </Modal>
     </>
   );
