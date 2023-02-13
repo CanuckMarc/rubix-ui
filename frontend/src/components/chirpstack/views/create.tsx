c3d2e7fdf84a67035a83f1701ce6514aa59ebb49
oanhhynguyen <52970495+nguyenoanh5597@users.noreply.github.com>
2023-02-13 13:21:34 +0700

    #695 create update device

--- frontend/src/components/chirpstack/views/create.tsx
+++ frontend/src/components/chirpstack/views/create.tsx
@@ -1,42 +1,48 @@
-import { Form, Input, Modal, Select } from "antd";
+import { Form, Input, Modal, Select, Spin } from "antd";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { chirpstack } from "../../../../wailsjs/go/models";
 import { ChirpFactory } from "../factory";
 
-import DeviceProfiles = chirpstack.DeviceProfiles;
 import Device = chirpstack.Device;
-import DevicesResult = chirpstack.DevicesResult;
 
 const formItemLayout = { labelCol: { span: 7 }, wrapperCol: { span: 15 } };
 
 export const CreateEditModal = (props: any) => {
   const { currentItem, isModalVisible, refreshList, onCloseModal } = props;
   const { connUUID = "", hostUUID = "" } = useParams();
-  const [confirmLoading, setConfirmLoading] = useState(false);
   const [form] = Form.useForm();
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [isDeviceProfilesFetching, setIsDeviceProfilesFetching] = useState(false);
+  const [deviceProfileOptions, setDeviceProfileOptions] = useState<any[]>([]);
   const [initData, setInitData] = useState({});
 
   const factory = new ChirpFactory();
 
-  const add = async (item: DevicesResult) => {
-    const payload = {} as Device;
-    await factory.CSAddDevice(connUUID, hostUUID, payload);
+  const add = async (device: Device) => {
+    await factory.CSAddDevice(connUUID, hostUUID, device);
   };
 
-  const editNetwork = async (item: DevicesResult) => {
-    const payload = {} as Device;
-    await factory.CSEditDevice(connUUID, hostUUID, item.devEUI, payload);
+  const edit = async (devEUI: string, device: Device) => {
+    await factory.CSEditDevice(connUUID, hostUUID, devEUI, device);
   };
 
   const handleClose = () => {
     onCloseModal();
   };
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (value: any) => {
     try {
       setConfirmLoading(true);
+      const payload = {} as Device;
+      payload.device = value;
+      if (currentItem) {
+        await edit(value.devEUI, payload);
+      } else {
+        await add(payload);
+      }
       refreshList();
+      handleClose();
     } catch (err) {
       console.log(err);
     } finally {
@@ -44,10 +50,37 @@ export const CreateEditModal = (props: any) => {
     }
   };
 
+  const fetchDeviceProfiles = async () => {
+    try {
+      setIsDeviceProfilesFetching(true);
+      const res = await factory.CSGetDeviceProfiles(connUUID, hostUUID);
+      if (res && res.result) {
+        const options = res.result.map((dp) => ({
+          value: dp.id,
+          label: dp.name,
+        }));
+        setDeviceProfileOptions(options);
+      }
+    } catch (err) {
+      setDeviceProfileOptions([]);
+    } finally {
+      setIsDeviceProfilesFetching(false);
+    }
+  };
+
   useEffect(() => {
-    if (isModalVisible) {
+    if (!isModalVisible) return;
+    if (currentItem) {
       setInitData({
-        deviceProfileID: "",
+        deviceProfileID: currentItem.deviceProfileID,
+        devEUI: currentItem.devEUI,
+        name: currentItem.name,
+        applicationID: currentItem.applicationID,
+        description: currentItem.description,
+      });
+    } else {
+      setInitData({
+        deviceProfileID: "31fcdf3c-efbc-439b-aaed-6acca398ecee",
         devEUI: "",
         name: "",
         applicationID: "",
@@ -57,6 +90,12 @@ export const CreateEditModal = (props: any) => {
   }, [isModalVisible]);
 
   useEffect(() => form.resetFields(), [initData]);
+
+  useEffect(() => {
+    if (!deviceProfileOptions || deviceProfileOptions.length === 0) {
+      fetchDeviceProfiles();
+    }
+  }, []);
 
   return (
     <>
@@ -71,35 +110,37 @@ export const CreateEditModal = (props: any) => {
         maskClosable={false}
         width={600}
       >
-        <Form {...formItemLayout} labelAlign="left" form={form} initialValues={initData} onFinish={handleSubmit}>
-          <Form.Item
-            label="Device Profile ID:"
-            name="deviceProfileID"
-            rules={[{ required: true, message: "Please select a Device Profile ID!" }]}
-          >
-            <Select showSearch={true} options={[]} />
-          </Form.Item>
-          <Form.Item
-            label="Device EUI:"
-            name="devEUI"
-            rules={[{ required: true, message: "Please input a device EUI!" }]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item label="Name:" name="name" rules={[{ required: true, message: "Please input a device name!" }]}>
-            <Input />
-          </Form.Item>
-          <Form.Item label="Application ID:" name="applicationID">
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Description:"
-            name="description"
-            rules={[{ required: true, message: "Please input a description!" }]}
-          >
-            <Input />
-          </Form.Item>
-        </Form>
+        <Spin spinning={isDeviceProfilesFetching}>
+          <Form {...formItemLayout} labelAlign="left" form={form} initialValues={initData} onFinish={handleSubmit}>
+            <Form.Item
+              label="Device Profile ID:"
+              name="deviceProfileID"
+              rules={[{ required: true, message: "Please select a Device Profile ID!" }]}
+            >
+              <Select showSearch={true} options={deviceProfileOptions} />
+            </Form.Item>
+            <Form.Item
+              label="Device EUI:"
+              name="devEUI"
+              rules={[{ required: true, message: "Please input a device EUI!" }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item label="Name:" name="name" rules={[{ required: true, message: "Please input a device name!" }]}>
+              <Input />
+            </Form.Item>
+            <Form.Item label="Application ID:" name="applicationID">
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Description:"
+              name="description"
+              rules={[{ required: true, message: "Please input a description!" }]}
+            >
+              <Input />
+            </Form.Item>
+          </Form>
+        </Spin>
       </Modal>
     </>
   );
