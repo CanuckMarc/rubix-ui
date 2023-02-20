d3f44c5ebe5b92dc5ccabd302e0eefeeb7d22e46
RaiBnod <binodrayee@gmail.com>
2023-02-20 20:23:39 +0545

    Add sync producers feature

--- backend/assistcli/ffproducer.go
+++ backend/assistcli/ffproducer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/NubeIO/nubeio-rubix-lib-models-go/pkg/v1/model"
 	"github.com/NubeIO/rubix-assist/service/clients/helpers/nresty"
+	"github.com/NubeIO/rubix-ui/backend/rumodel"
 	"github.com/NubeIO/rubix-ui/backend/utils/urls"
 )
 
@@ -101,4 +102,18 @@ func (inst *Client) DeleteProducer(hostIDName, uuid string) (bool, error) {
 		return false, err
 	}
 	return true, nil
+}
+
+func (inst *Client) SyncProducers(hostIDName, streamUUID string) (*[]rumodel.SyncModel, error) {
+	url := fmt.Sprintf(
+		"proxy/ff/api/streams/%s/sync/producers?with_writer_clones=true", streamUUID)
+	resp, err := nresty.FormatRestyResponse(inst.Rest.R().
+		SetHeader("host-uuid", hostIDName).
+		SetHeader("host-name", hostIDName).
+		SetResult(&[]rumodel.SyncModel{}).
+		Get(url))
+	if err != nil {
+		return nil, err
+	}
+	return resp.Result().(*[]rumodel.SyncModel), nil
 }
